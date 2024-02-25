4773e5370be1b965d05c841a0b26100bd85eb65c
thainq34 <nqthai03@gmail.com>
2024-02-25 18:42:39 +0700

    [COMP1640] Change SQL Server connection

--- Utils/DataContext.cs
+++ Utils/DataContext.cs
@@ -12,7 +12,10 @@ namespace COMP1640.Utils
             //local connect: Remember to uncomment this to use your database at local
             //optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS02;Database=COMP1640;Trusted_Connection=True;TrustServerCertificate=true");
             //stg connect: Remember to uncomment this when you want to use your database at stg
-            optionsBuilder.UseSqlServer("Server=tcp:comp1640-stg.database.windows.net,1433;Initial Catalog=comp1640-stg;Persist Security Info=False;User ID=comp1640-stg;Password=Azuredbstaging252;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //optionsBuilder.UseSqlServer("Server=tcp:comp1640-stg.database.windows.net,1433;Initial Catalog=comp1640-stg;Persist Security Info=False;User ID=comp1640-stg;Password=Azuredbstaging252;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //prod connect: Connect to production environment
+            optionsBuilder.UseSqlServer("Server=tcp:comp1640-prod.database.windows.net,1433;Initial Catalog=comp1640-prod;Persist Security Info=False;User ID=comp1640-prod;Password=Azuredb252;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
         }
         public DbSet<User> User { get; set; }
     }
