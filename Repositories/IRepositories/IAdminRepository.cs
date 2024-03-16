b0ce17cbb7ea6fc979450e953121dd91c9d5b483
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-03-16 15:13:50 +0700

    [List User] API list user

---
+++ Repositories/IRepositories/IAdminRepository.cs
@@ -0,0 +1,9 @@
+﻿using COMP1640.Models;
+
+namespace COMP1640.Repositories.IRepositories
+{
+    public interface IAdminRepository
+    {
+        public IEnumerable<User> GetAllUsers();
+    }
+}
---
+++ Repositories/Services/AdminService/AdminService.cs
@@ -0,0 +1,21 @@
+﻿using COMP1640.Models;
+using COMP1640.Repositories.IRepositories;
+using COMP1640.Utils;
+
+namespace COMP1640.Repositories.Services.AdminService
+{
+    public class AdminService : IAdminRepository
+    {
+
+        private readonly DataContext _dataContext;
+        public AdminService(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _dataContext.User.ToList();
+                                    
+        }
+    }
+}
