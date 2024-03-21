a5422c039f9b79210e668200974e7f95b2d97ea3
Nguyen Vu Quang Minh <vuminh394@gmail.com>
2024-03-21 21:15:53 +0700

    API for Faculty

---
+++ Models/Faculty.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP1640.Models
+{
+    public class Faculty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public List<User> User { get; set; }
+        public List<Topic> Topics { get; set; }
+
+        public string Department { get; set; }
+    }
+}
