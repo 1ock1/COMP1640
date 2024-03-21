6ca652d7a889498f8f5bdee9c8f8c6190991750e
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-03-21 22:05:05 +0700

    CRU API for Admin-Topic-Academic

---
+++ Controllers/TopicController.cs
@@ -0,0 +1,77 @@
+﻿using COMP1640.Models;
+using COMP1640.Repositories.IRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static COMP1640.DTOs.TopicDTOs;
+
+namespace COMP1640.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly ITopicRepository _topicRepository;
+
+        public TopicController(ITopicRepository topicRepository)
+        {
+            this._topicRepository = topicRepository;
+        }
+
+
+
+        [HttpGet("GetAllTopic")]
+        public IEnumerable<Topic> GetAllTopics()
+        {
+            // Call the service to get all topics
+            IEnumerable<Topic> topics = _topicRepository.GetAllTopics();
+            return topics;
+        }
+
+        [HttpGet("GetTopicById")]
+        public Topic GetTopicById(int id)
+        {
+            // Call the service to get topic by id
+            Topic topic = _topicRepository.GetTopicById(id);
+            return topic;
+        }
+
+        [HttpPost("CreateTopic")]
+        public IActionResult CreateTopic(CreateTopic topic)
+        {
+            // Call the service to create topic
+            Topic createTopic = new Topic();
+            createTopic.Name = topic.Name;
+            createTopic.Description = topic.Description;
+            createTopic.EntriesDate = topic.FinalDate;
+            createTopic.FinalDate = topic.FinalDate;
+
+            createTopic.FalcutyId = 1;
+            createTopic.AcademicId = 1;
+
+
+
+
+            return _topicRepository.CreateTopic(createTopic);
+        }
+
+        [HttpPut("UpdateTopic")]
+        public IActionResult UpdateTopic(int id, UpdateTopic topic)
+        {
+            // Call the service to update topic
+            Topic topicInformation = _topicRepository.GetTopicById(id);
+            topicInformation.Name = topic.Name ?? topicInformation.Name;
+            topicInformation.Description = topic.Description ?? topicInformation.Description;
+            topicInformation.EntriesDate = topic.FinalDate;
+            topicInformation.FinalDate = topic.FinalDate;
+            return _topicRepository.UpdateTopic(topicInformation);
+        }
+
+        [HttpDelete("DeleteTopic")]
+
+        public IActionResult DeleteTopic(int id)
+        {
+            // Call the service to delete topic
+            return _topicRepository.DeleteTopic(id);
+        }
+    }
+}
---
+++ DTOs/AcademicDTOs.cs
@@ -0,0 +1,17 @@
+﻿namespace COMP1640.DTOs
+{
+    public class AcademicDTOs
+    {
+        public class  CreateAcademic
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
+        public class UpdateAcademic
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+    }
+}
