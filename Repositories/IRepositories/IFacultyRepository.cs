621cd79751c336c1deab406cf01dcd209a7d1991
Nguyen Vu Quang Minh <vuminh394@gmail.com>
2024-03-22 17:18:23 +0700

    API for Faculty

---
+++ Repositories/IRepositories/IFacultyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using COMP1640.Models;
+
+namespace COMP1640.Repositories.IRepositories
+{
+    public interface IFacultyRepository
+    {
+        IEnumerable<Faculty> GetAllFaculties();
+        Faculty GetFacultyById(int id);
+        void CreateFaculty(Faculty faculty);
+        void UpdateFaculty(int id, Faculty faculty);
+        void DeleteFaculty(int id);
+    }
+}
