d6eb88657973283c0904d4539a17278a539450c7
Nguyen Vu Quang Minh <vuminh394@gmail.com>
2024-03-23 15:03:14 +0700

    Finished DTO and Service for Faculty

---
+++ DTOs/FacultyDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace COMP1640.DTOs
+{
+        public class CreateFacultyDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool Status { get; set; }
+            public string Description { get; set; }
+            // Add more properties as needed
+        }
+
+        public class EditFacultyDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool Status { get; set; }
+            public string Description { get; set; }
+        }
+}
