dec68238ce7bad969d540aa443a14f0da30cbdfd
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-03-24 01:51:33 +0700

    Fix Falcuty

--- Repositories/Services/FacultyService/FacultyService.cs
+++ Repositories/Services/FacultyService/FacultyService.cs
@@ -1,59 +1,50 @@
 ﻿using COMP1640.Models;
 using COMP1640.Repositories.IRepositories;
-using COMP1640.DTOs;
-using System;
-using System.Collections.Generic;
+using COMP1640.Utils;
 
-namespace COMP1640.Services {
-            public class FacultyService
-            {
-                private readonly IFacultyRepository _facultyRepository;
 
-                public FacultyService(IFacultyRepository facultyRepository)
-                {
-                    _facultyRepository = facultyRepository;
-                }
+namespace COMP1640.Services
+{
+    public class FacultyService : IFacultyRepository
+    {
+        private readonly DataContext _dataContext;
+        public FacultyService(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
 
-                public IEnumerable<Falcuty> GetAllFaculties()
-                {
-                    return _facultyRepository.GetAllFaculties();
-                }
+        public string CreateFaculty(Falcuty faculty)
+        {
+            _dataContext.Falcuties.Add(faculty);
+            _dataContext.SaveChanges();
+            return "Faculty created successfully";
+        }
 
-                public Falcuty GetFacultyById(int id)
-                {
-                    return _facultyRepository.GetFacultyById(id);
-                }
+        public string DeleteFaculty(int id)
+        {
+            _dataContext.Falcuties.Remove(_dataContext.Falcuties.Find(id));
+            _dataContext.SaveChanges();
+            return "Faculty deleted successfully";
+        }
 
-                public void CreateFaculty(CreateFacultyDto facultyDto)
-                {
-                    var faculty = new Falcuty
-                    {
-                        Name = facultyDto.Name,
-                        Status = facultyDto.Status,
-                    };
-                    _facultyRepository.CreateFaculty(faculty);
-                }
+        public IEnumerable<Falcuty> GetAllFaculties()
+        {
+            return _dataContext.Falcuties.ToList();
+        }
 
-                public void UpdateFaculty(int id, EditFacultyDto facultyDto)
-                {
-                    // You can add business logic/validation here before updating faculty
-                    var existingFaculty = _facultyRepository.GetFacultyById(id);
-                    if (existingFaculty == null)
-                    {
-                        throw new ArgumentException("Faculty not found");
-                    }
+        public Falcuty GetFacultyById(int id)
+        {
+            return _dataContext.Falcuties.Find(id);
+        }
 
-                    existingFaculty.Name = facultyDto.Name;
-                    existingFaculty.Status = facultyDto.Status;
-                    // Map other properties as needed
+        public string UpdateFaculty(int id, Falcuty faculty)
+        {
+            _dataContext.Falcuties.Update(faculty);
+            _dataContext.SaveChanges();
+            return "Faculty updated successfully";
+        }
 
-                    _facultyRepository.UpdateFaculty(id, existingFaculty);
-                }
 
-                public void DeleteFaculty(int id)
-                {
-                    // You can add business logic/validation here before deleting faculty
-                    _facultyRepository.DeleteFaculty(id);
-                }
-            }
+
+    }
 }
